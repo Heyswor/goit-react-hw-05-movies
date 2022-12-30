0657326087f8bc3a8860048c8768a397d7e943fa
Heyswor <andreydymov93@gmail.com>
2022-12-30 10:08:29 +0200

    hw5-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { Home } from 'pages/Home';
 import { Movies } from 'pages/Movies';
-import { MovieDetails } from './MovieDetails/MovieDetails';
-import { Cast } from './MovieCredits/MovieCredits';
-import { Reviews } from './MovieReviews/MovieReviews';
+// import { MovieDetails } from './MovieDetails/MovieDetails';
+// import { Cast } from './MovieCredits/MovieCredits';
+// import { Reviews } from './MovieReviews/MovieReviews';
 import { lazy, Suspense } from 'react';
 
-// const MovieDetails = lazy(() =>
-//   import('../components/MovieDetails/MovieDetails')
-// );
-// const Cast = lazy(() => import('../components/MovieCredits/MovieCredits'));
-// const Reviews = lazy(() => import('../components/MovieReviews/MovieReviews'));
+const MovieDetails = lazy(() =>
+  import('../components/MovieDetails/MovieDetails')
+);
+const Cast = lazy(() => import('../components/MovieCredits/MovieCredits'));
+const Reviews = lazy(() => import('../components/MovieReviews/MovieReviews'));
 
 export const App = () => {
   return (
@@ -23,9 +23,9 @@ export const App = () => {
         <Route
           path="/movies/:movieId"
           element={
-            // <Suspense>
-            <MovieDetails />
-            // </Suspense>
+            <Suspense>
+              <MovieDetails />
+            </Suspense>
           }
         >
           <Route path="cast" element={<Cast />} />
